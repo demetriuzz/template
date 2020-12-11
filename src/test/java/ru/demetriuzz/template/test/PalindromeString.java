1aa217d78037e834e06b43aecaf506163b917bab
Дмитрий В. Лысцов <Lystsov_D@ptspec.ru>
2020-12-11 10:35:54 +0500

    PalindromeString: реверс строки через рекурсию

--- src/test/java/ru/demetriuzz/template/test/PalindromeString.java
+++ src/test/java/ru/demetriuzz/template/test/PalindromeString.java
@@ -15,6 +15,15 @@ public class PalindromeString {
         Assertions.assertTrue(verify1("каЗак"));
         Assertions.assertTrue(verify1("trArt"));
         Assertions.assertFalse(verify1("qweasd"));
+
+        Assertions.assertFalse(verify2(null));
+        Assertions.assertFalse(verify2(""));
+        Assertions.assertFalse(verify2("   "));
+
+        Assertions.assertTrue(verify2("12321"));
+        Assertions.assertTrue(verify2("каЗак"));
+        Assertions.assertTrue(verify2("trArt"));
+        Assertions.assertFalse(verify2("qweasd"));
     }
 
     private boolean verify1(String in) {
@@ -22,6 +31,21 @@ public class PalindromeString {
 
         // ? trim()
         return in.equalsIgnoreCase(new StringBuilder(in).reverse().toString());
+    }
+
+
+    private boolean verify2(String in) {
+        if (in == null || in.isEmpty() || in.isBlank()) return false;
+
+        return in.equalsIgnoreCase(reverseString(in));
+    }
+
+    private String reverseString(String str) {
+        // остался один символ, начало процесса свертывания ..
+        if (str.length() <= 1) return str;
+
+        // строка с индекса 1 + первый символ (переворот)
+        return reverseString(str.substring(1)) + str.charAt(0);
     }
 
 }
