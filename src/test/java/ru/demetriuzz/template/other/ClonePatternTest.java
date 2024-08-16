4095d2ac839b8c0711b39876541a28d1c5f89622
Dmitrii V. Lystsov <dlystsov@softmedialab.com>
2024-08-16 13:37:20 +0500

    рефакторинг, начало нового теста EqualHashCodeTest

--- src/test/java/ru/demetriuzz/template/other/ClonePatternTest.java
+++ src/test/java/ru/demetriuzz/template/other/ClonePatternTest.java
@@ -2,7 +2,7 @@ package ru.demetriuzz.template.other;
 
 import org.junit.jupiter.api.Test;
 
-public class ClonePatternExample {
+public class ClonePatternTest {
 
     private static class MObject implements Cloneable {
 
