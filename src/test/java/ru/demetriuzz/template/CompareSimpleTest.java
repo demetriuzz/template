418fd52f1cfdc31a65c47ac1d7b4509029e00bf4
Dmitrii V. Lystsov <demetriuzz@ya.ru>
2024-11-10 19:12:34 +0500

    обновление теста сравнения

--- src/test/java/ru/demetriuzz/template/CompareSimpleTest.java
+++ src/test/java/ru/demetriuzz/template/CompareSimpleTest.java
@@ -12,16 +12,19 @@ public class CompareSimpleTest {
         final var so11 = SimpleObject.builder().id(10).name("имя 10").flag(true).coast(BigDecimal.ONE).build();
         final var so12 = SimpleObject.builder().id(10).name("имя 10").flag(true).coast(BigDecimal.ONE).build();
         Assertions.assertEquals(so12, so11);
+        Assertions.assertEquals(so11, so12);
         Assertions.assertEquals(so12.hashCode(), so11.hashCode());
         System.out.printf("1. so11.hash=%d, so12.hash=%d %n%n", so11.hashCode(), so12.hashCode());
 
         so12.setId(12);
         Assertions.assertNotEquals(so12, so11);
+        Assertions.assertNotEquals(so11, so12);
         Assertions.assertNotEquals(so12.hashCode(), so11.hashCode());
         System.out.printf("2. so11.hash=%d, so12.hash=%d %n%n", so11.hashCode(), so12.hashCode());
 
         final var so13 = SimpleObject.builder().id(12).name("имя 10").flag(true).coast(BigDecimal.ONE).build();
         Assertions.assertEquals(so13, so12);
+        Assertions.assertEquals(so12, so13);
         Assertions.assertEquals(so13.hashCode(), so12.hashCode());
         System.out.printf("3. so12.hash=%d, so13.hash=%d %n%n", so12.hashCode(), so13.hashCode());
     }
